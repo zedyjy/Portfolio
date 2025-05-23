378d97c10cb1e431e8155f8ce5e1ba4b827caf3f
zedyjy <doga.dellal@ug.bilkent.edu.tr>
2025-05-24 00:23:59 +0300

    fix: change repo and website name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: "/portfolio-website/", // ← change to your repo name
+    base: "/Portfolio/", // ← change to your repo name
   plugins: [react()],
 })
