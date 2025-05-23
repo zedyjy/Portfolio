fb71c2088df259e0563dc744130cb2b7b9ab995b
zedyjy <doga.dellal@ug.bilkent.edu.tr>
2025-05-24 00:21:30 +0300

    fix: delete unnecessary files

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: "/portfolio-website/", // ← change to your repo name
   plugins: [react()],
 })
